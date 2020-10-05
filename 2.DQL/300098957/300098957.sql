3f671b29778e3428e351637810ec08ec1b8e204c
Brice <62551735+b300098957@users.noreply.github.com>
2020-10-05 16:49:23 -0400

    Update 300098957.sql

--- 2.DQL/300098957/300098957.sql
+++ 2.DQL/300098957/300098957.sql
@@ -2,4 +2,14 @@
 
 SELECT * FROM film JOIN film_actor USING (film_id);
 
--- IMprimer ..
+-- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
+
+-- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+
+-- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algerie (ANSI-92)
+
+-- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
+
+-- Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
+
+-- Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
