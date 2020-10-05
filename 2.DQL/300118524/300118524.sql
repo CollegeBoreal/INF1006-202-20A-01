93cfe4c0876662b9d2ad08a245df7bde6903a8fc
zoureni <56364857+zoureni@users.noreply.github.com>
2020-10-05 17:30:19 -0400

    Create 300118524.sql

---
+++ 2.DQL/300118524/300118524.sql
@@ -0,0 +1,17 @@
+-- Ma requete SQL
+
+SELECT * FROM film JOIN film_actor USING (film_id);
+
+-- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
+
+SELECT city, country FROM city JOIN country USING (country_id) WHERE city like 'Lu%';	
+
+-- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+
+-- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algerie (ANSI-92)
+
+-- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
+
+-- Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
+
+-- Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
