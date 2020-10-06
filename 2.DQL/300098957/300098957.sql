b16230d5a5fa430483c28da0c0a1d1bd1eb250bd
Brice <62551735+b300098957@users.noreply.github.com>
2020-10-06 16:07:31 -0400

    Update 300098957.sql

--- 2.DQL/300098957/300098957.sql
+++ 2.DQL/300098957/300098957.sql
@@ -4,11 +4,12 @@ SELECT * FROM film JOIN film_actor USING (film_id);
 
 -- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
 
-SELECT city.city, customer.last_name, payment.amount FROM city 
+SELECT city.city, customer.last_name, SUM(payment.amount) FROM city 
 JOIN address USING (city_id) 
 JOIN customer USING (address_id)
 JOIN payment USING (customer_id)
-WHERE city = "Lubumbashi";
+WHERE city = "Lubumbashi"
+GROUP BY city.city, customer.last_name;
 
 -- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
 
