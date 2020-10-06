5bea7049f19355c8ebf25d55a0ef09a77803f9db
Zack <54910329+ZackTO@users.noreply.github.com>
2020-10-06 16:50:57 -0400

    Update 300115140.sql

--- 2.DQL/300115140/300115140.sql
+++ 2.DQL/300115140/300115140.sql
@@ -7,7 +7,6 @@ JOIN payment USING (customer_id)
 WHERE city = "Lubumbashi"
 GROUP BY city.city, customer.last_name;
 
-
 -- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
 
 SELECT country.country, customer.last_name, SUM(payment.amount) FROM country
@@ -18,12 +17,18 @@ JOIN payment USING (customer_id)
 WHERE country = "Congo%" OR country = "Algeria"
 GROUP BY country.country, customer.last_name;
 
-
-
-
 -- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algerie (ANSI-92)
 
 
 -- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
+
+SELECT film.film_id, actor.actor_id FROM film
+JOIN film_actor USING film_id
+JOIN actor USING actor_id
+GROUP BY actor.last_name
+ORDER BY COUNT (*)
+LIMIT 1;
+
+
 -- Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
 -- Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
