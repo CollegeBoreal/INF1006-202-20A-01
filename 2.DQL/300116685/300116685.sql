45487e9eabbee897f8c5a917935c556c430c7572
amirali175 <54910751+amirali175@users.noreply.github.com>
2020-10-06 17:58:18 -0400

    Update 300116685.sql

--- 2.DQL/300116685/300116685.sql
+++ 2.DQL/300116685/300116685.sql
@@ -4,6 +4,13 @@ SELECT * FROM sakila.actor where first_name = 'JENNIFER ' ;
 --Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
 SELECT city, country FROM city JOIN country USING (country_id) WHERE ville like  ' Lu% ' ;
 --Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+SELECT country.country, customer.first_name, payment.amount
+from country  
+JOIN city using (country_id)
+JOIN address using (city_id)
+JOIN customer using (address_id)
+JOIN payment using (customer_id)
+WHERE country LIKE "Congo" OR "Algerie"
 --Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algérie (ANSI-92)
 --Quel (le) acteur (riz) est apparu (e) dans le plus de films?
 --Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
