0bddb5f1047de1219d8ea8bd436b608076b3aa2d
amirali175 <54910751+amirali175@users.noreply.github.com>
2020-10-06 08:21:04 -0400

    Update 300116685.sql

--- 2.DQL/300116685/300116685.sql
+++ 2.DQL/300116685/300116685.sql
@@ -1,2 +1,8 @@
 --ma premiere requete-- 
 SELECT * FROM sakila.actor;
+Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
+Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algérie (ANSI-92)
+Quel (le) acteur (riz) est apparu (e) dans le plus de films?
+Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
+Quels sont les catégories qui ont les plus longs films (ie dépassant la longueur moyenne)? Trié par longueur moyenne
