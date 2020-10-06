8adb215be4b4d57d07250c6dd32acdd50d34fea3
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2020-10-06 16:12:19 -0400

    :smile:

--- 2.DQL/300116973/300116973.sql
+++ 2.DQL/300116973/300116973.sql
@@ -6,7 +6,12 @@ SELECT * FROM sakila.actor where first_name = 'NICK';
 
 -- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
 
-SELECT city, country FROM city JOIN country using (country_id) WHERE city like "%L";
+SELECT city.city, customer.last_name, SUM(payment.amount) FROM city 
+JOIN address USING (city_id) 
+JOIN customer USING (address_id)
+JOIN payment USING (customer_id)
+WHERE city = "Lubumbashi"
+GROUP BY city.city, customer.last_name;
 
 -- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
 
