e63affef7e4306a18ccc22a7daa8b23a670bb31b
amirali175 <54910751+amirali175@users.noreply.github.com>
2020-10-06 16:33:15 -0400

    Update 300116685.sql

--- 2.DQL/300116685/300116685.sql
+++ 2.DQL/300116685/300116685.sql
@@ -1,10 +1,10 @@
 --ma premiere requete-- 
 SELECT * FROM sakila.actor;
 SELECT * FROM sakila.actor where first_name = 'JENNIFER ' ;
-Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
+--Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
 SELECT city, country FROM city JOIN country USING (country_id) WHERE ville like  ' Lu% ' ;
-Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
-Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algérie (ANSI-92)
-Quel (le) acteur (riz) est apparu (e) dans le plus de films?
-Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
-Quels sont les catégories qui ont les plus longs films (ie dépassant la longueur moyenne)? Trié par longueur moyenne
+--Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+--Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algérie (ANSI-92)
+--Quel (le) acteur (riz) est apparu (e) dans le plus de films?
+--Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
+--Quels sont les catégories qui ont les plus longs films (ie dépassant la longueur moyenne)? Trié par longueur moyenne
