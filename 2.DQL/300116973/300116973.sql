2986b24f2f8b43a4a1ae12a12ee51e0859053722
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2020-10-07 20:23:14 -0400

    :car::smile::dog::smile:

--- 2.DQL/300116973/300116973.sql
+++ 2.DQL/300116973/300116973.sql
@@ -1,6 +1,6 @@
 -- Ma requette SQL
 
-SELECT * FROM sakila.customer join sakila.film_actor
+SELECT * FROM sakila.customer join sakila.film_actor;
 SELECT * FROM sakila.actor where first_name = 'NICK';
 
 -- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
