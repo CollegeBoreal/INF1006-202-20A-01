7ba17a007c89a8caa5f82e08e06b535dd1228978
300111824 <54911706+300111824@users.noreply.github.com>
2020-10-10 14:49:33 -0400

    Update 300111824.sql

--- 2.DQL/300111824/300111824.sql
+++ 2.DQL/300111824/300111824.sql
@@ -3,6 +3,13 @@
 
 SELECT * FROM sakila.country join sakila.film_country USING (film_id);
 -- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
+SELECT city.city, customer.first_name, SUM(payment.amount)
+FROM city
+JOIN address USING (city_id)
+JOIN customer USING (address_id)
+JOIN payment USING (customer_id)
+WHERE city = "Lubumbashi"
+GROUP BY city.city, customer.first_name;
 -- Imprimer le nom et le montant de factures des personnes habitant le Congo ou l'algerie (ANSI-92)
 -- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo ou l'Algerie (ANSI-92)
 -- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
