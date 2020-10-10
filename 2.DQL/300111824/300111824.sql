03fa2a845cafb4350c310ef6e5c654bf34aa84d2
300111824 <54911706+300111824@users.noreply.github.com>
2020-10-10 14:11:24 -0400

    Update 300111824.sql

--- 2.DQL/300111824/300111824.sql
+++ 2.DQL/300111824/300111824.sql
@@ -1,4 +1,10 @@
   
 --Marequete sql
 
-SELECT * FROM sakila.country join sakila.film_country USING (film_id)
+SELECT * FROM sakila.country join sakila.film_country USING (film_id);
+-- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
+-- Imprimer le nom et le montant de factures des personnes habitant le Congo ou l'algerie (ANSI-92)
+-- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo ou l'Algerie (ANSI-92)
+-- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
+-- Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
+-- Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
