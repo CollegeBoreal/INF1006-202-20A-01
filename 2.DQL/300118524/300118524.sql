73070d2dd516ff1cd10ca72252f0ff9d9f06df7f
zoureni <56364857+zoureni@users.noreply.github.com>
2020-10-18 13:40:49 -0400

    Update 300118524.sql

--- 2.DQL/300118524/300118524.sql
+++ 2.DQL/300118524/300118524.sql
@@ -3,8 +3,6 @@
 SELECT * FROM film JOIN film_actor USING (film_id);
 
 -- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
---SELECT city, country FROM city JOIN country USING (country_id) WHERE city like 'Lu%';	
-
 SELECT city.city, customer.last_name, SUM(payment.amount) FROM city 
 JOIN address USING (city_id) 
 JOIN customer USING (address_id)
@@ -12,8 +10,18 @@ JOIN payment USING (customer_id)
 WHERE city = "Lubumbashi"
 GROUP BY city.city, customer.last_name;
 
--- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
 
+-- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+SELECT country.country, customer.last_name, payment.amount FROM country 
+JOIN city USING (country_id)
+JOIN address USING (city_id) 
+JOIN customer USING (address_id)
+JOIN payment USING (customer_id)
+WHERE country LIKE 'Con%'
+OR country LIKE 'Alger%';
+
+
+-- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algerie (ANSI-92)
 SELECT country.country, customer.last_name, SUM(payment.amount)FROM country 
 JOIN city USING (country_id)
 JOIN address USING (city_id) 
@@ -23,10 +31,29 @@ WHERE country LIKE 'Con%'
 OR country LIKE 'Alger%'
 GROUP BY country.country, customer.last_name;
 
--- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algerie (ANSI-92)
 
 -- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
+-- https://www.w3resource.com/sql/aggregate-functions/count-with-group-by.php
+SELECT actor.last_name, COUNT(*)
+FROM film
+JOIN film_actor USING (film_id)
+JOIN actor USING  (actor_id)
+GROUP BY actor.last_name 
+ORDER BY 2 DESC
+LIMIT 1;
+
 
 -- Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
+SELECT category.name, AVG(film.length)
+FROM film
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+GROUP BY category.name;
+
 
 -- Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
+SELECT AVG(length) ,name FROM film 
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+GROUP BY name
+HAVING AVG (length) > (SELECT AVG(length) from film);
