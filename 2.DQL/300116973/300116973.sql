baa98a7c260ae126edb124f0093a4115e1c4fd3c
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2020-10-18 11:09:05 -0400

    :car::smile::dog::smile:

--- 2.DQL/300116973/300116973.sql
+++ 2.DQL/300116973/300116973.sql
@@ -14,7 +14,7 @@ GROUP BY city.city, customer.last_name;
 
 -- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
 
-SELECT country.country, customer.first_name, payment.amount
+SELECT country.country, customer.last_name, payment.amount
 FROM country
 JOIN city USING (country_id) 
 JOIN address USING (city_id) 
@@ -24,22 +24,22 @@ WHERE country = "Algeria" OR country LIKE "Congo%";
 
 -- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo et l'Algerie (ANSI-92)
 
-SELECT country.country, customer.first_name, SUM(payment.amount)
+SELECT country.country, customer.last_name, SUM(payment.amount)
 FROM country
 JOIN city USING (country_id) 
 JOIN address USING (city_id) 
 JOIN customer USING (address_id)
 JOIN payment USING (customer_id)
 WHERE country = "Algeria" OR country LIKE "Congo%"
-GROUP BY country.country, customer.first_name;
+GROUP BY country.country, customer.last_name;
 
 -- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
 
-SELECT actor.first_name,  COUNT(*)
+SELECT actor.last_name,  COUNT(*)
 FROM film
 JOIN film_actor USING (film_id) 
 JOIN actor USING (actor_id) 
-GROUP BY actor.first_name
+GROUP BY actor.last_name
 LIMIT 1;
 
 -- Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
@@ -48,7 +48,8 @@ SELECT name AS categorie, AVG(length) AS long_moyenne
 FROM film
 JOIN film_category USING (film_id)
 JOIN category USING (category_id)
-GROUP BY name;
+GROUP BY name
+ORDER BY AVG(length)  ;
  
 -- Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
 
@@ -59,6 +60,7 @@ JOIN film_category USING (film_id)
 JOIN category USING (category_id)
 GROUP BY name
 HAVING (SELECT COUNT(film.length)
-FROM film) > AVG(film.length) ;
+FROM film) > AVG(film.length) 
+ORDER BY AVG(film.length) ;
  
 
