cdd7489390cd0ceb0312227f9ba8416bbfc8ab66
morti747 <54910700+morti747@users.noreply.github.com>
2020-10-19 00:08:31 -0400

    Update 300117314.sql

--- 2.DQL/300117314/300117314.sql
+++ 2.DQL/300117314/300117314.sql
@@ -11,6 +11,13 @@ GROUP BY city, customer.last_name;
 -- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
 
 
+SELECT country, customer.last_name, SUM(payment.amount) FROM country
+JOIN city USING (country_id)
+JOIN address USING (city_id)
+JOIN customer USING (address_id)
+JOIN payment USING (customer_id)
+WHERE country like "Congo%" OR "Algeri%"
+GROUP BY country, customer.last_name;
 
 
 
