08f99df9fc81073bfdf9d5263640d8c4c0e401e0
morti747 <54910700+morti747@users.noreply.github.com>
2020-10-18 23:09:17 -0400

    Update 300117314.sql

--- 2.DQL/300117314/300117314.sql
+++ 2.DQL/300117314/300117314.sql
@@ -1,4 +1,4 @@
-
+-- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
 
 SELECT city,customer.last_name, SUM(payment.amount) FROM city
 JOIN address USING (city_id)
@@ -6,3 +6,17 @@ JOIN customer USING (address_id)
 JOIN payment USING (customer_id)
 WHERE city='lubumbashi'
 GROUP BY city, customer.last_name;
+
+
+-- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+
+
+
+
+
+
+
+
+
+
+
