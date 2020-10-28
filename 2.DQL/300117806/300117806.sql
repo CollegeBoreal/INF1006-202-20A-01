45abf507057855a7520fa6925852c41089a60ec1
djibo648 <54910103+djibo648@users.noreply.github.com>
2020-10-28 14:33:28 -0400

    Update 300117806.sql

--- 2.DQL/300117806/300117806.sql
+++ 2.DQL/300117806/300117806.sql
@@ -1,1 +1,53 @@
+Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
+
+SELECT city.city, customer.last_name, SUM(payment.amount)  FROM city 
+JOIN address USING (city_id)
+JOIN customer USING (address_id)
+JOIN payment USING (customer_id)
+WHERE city = 'Lubumbashi'
+GROUP BY city.city, customer.last_name
+
+
+
+
+Imprimer le nom et le montant de factures des personnes habitant le Congo ou l'algerie (ANSI-92)
+
+
+
+
+
+
+
+
+Imprimer le nom et le montant de la facture totale des personnes habitant le Congo ou l'Algerie (ANSI-92)
+
+
+
+
+
+
+
+
+
+Quel(le) acteur(rice) est apparu(e) dans le plus de films?
+
+
+
+
+
+
+
+
+Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
+
+
+
+
+
+
+
+
+
+Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
+
 
