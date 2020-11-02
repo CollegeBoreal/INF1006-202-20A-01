5960663edfddc924b9d496d81b046fda1287e576
Brice <62551735+b300098957@users.noreply.github.com>
2020-11-02 17:46:05 -0500

    Update 300098957-schema.sql

--- 3.DDL-DCL/300098957/300098957-schema.sql
+++ 3.DDL-DCL/300098957/300098957-schema.sql
@@ -3,7 +3,9 @@ CREATE DATABASE fournisseurs_et_clients;
 
 USE fournisseurs_et_clients;
 
-
+-- ***********************
+-- Ma table fournisseurs
+-- ***********************
 CREATE TABLE fournisseurs (
     noms VARCHAR(20)
     );
