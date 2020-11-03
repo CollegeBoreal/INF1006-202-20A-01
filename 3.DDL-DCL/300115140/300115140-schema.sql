67027971e11e0a863617fde5b9479741e01fddbf
Zack <54910329+ZackTO@users.noreply.github.com>
2020-11-02 22:35:15 -0500

    Update 300115140-schema.sql

--- 3.DDL-DCL/300115140/300115140-schema.sql
+++ 3.DDL-DCL/300115140/300115140-schema.sql
@@ -3,7 +3,35 @@
 -- Création de ma DATABASE
 -- *********************************
 
+CREATE DATABASE ETREBIEN;
+
 -- *********************************
---  Création de la TABLE 
+--  Création des TABLES 
 -- ********************************
+
+CREATE TABLE Fournisseurs (
+  Nom VARCHAR(20),
+  Adresse VARCHAR(35)
+  Telephone INT(10)
+  Nature_du_fournisseur VARCHAR(15)
+  );
+
+CREATE TABLE Ventes (
+  Services VARCHAR(15),
+  Produits VARCHAR(15),
+  );
+  
+CREATE TABLE Clients (
+  Nom VARCHAR(20),
+  Prenom VARCHAR(20),
+  Adresse VARCHAR(35),
+  Code_Postale VARCHAR(6),
+  Telephone INT,
+  Clients_venant_de VARCHAR(15),
+  );
+  
+  
+  
+
+
 
