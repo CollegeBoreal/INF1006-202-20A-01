d996446df5c54505c81147c1dfdf84a2d6162480
Zack <54910329+ZackTO@users.noreply.github.com>
2020-11-03 18:04:47 -0500

    Update 300115140-schema.sql

--- 3.DDL-DCL/300115140/300115140-schema.sql
+++ 3.DDL-DCL/300115140/300115140-schema.sql
@@ -19,9 +19,9 @@ USE ETREBIEN;
 
 CREATE TABLE Fournisseur (
   Id_fournisseur INT AUTO_INCREMENT,
-  Nom VARCHAR(20),
+  Nom VARCHAR(20) NOT NULL,
   Adresse VARCHAR(35),
-  Telephone INT,
+  Telephone INT NOT NULL,
   Nature_du_fournisseur VARCHAR(15)
   PRIMARY KEY (Id_fournisseur)
   );
@@ -30,32 +30,32 @@ CREATE TABLE Ventes (
   id_ventes INT AUTO_INCREMENT,
   id_clients INT,
   Type VARCHAR (15),
-  Prix INT,
+  Prix INT NOT NULL,
   Date_de_vente DATE
   PRIMARY KEY (id_ventes, id_clients)
   );
   
 CREATE TABLE Clients (
   id_client
-  Nom VARCHAR(20),
+  Nom VARCHAR(20) NOT NULL,
   Prenom VARCHAR(20),
   Adresse VARCHAR(35),
   Code_postale VARCHAR (10),
-  Telephone VARCHAR(10),
+  Telephone VARCHAR(10) NOT NULL,
   PRIMARY KEY (id_client)
   );
   
  CREATE TABLE Produits (
    id_produit INT AUTO_INCREMENT,
    id_client INT,
-   Nom_du_produit VARCHAR(15),
+   Nom_du_produit VARCHAR(15) NOT NULL,
    PRIMARY KEY (id_produit, id_client)
    );
    
   CREATE TABLE Services (
     id_services INT AUTO_INCREMENT,
     id_fournisseur INT 
-    Nom_du_service VARCHAR(15),
+    Nom_du_service VARCHAR(15) NOT NULL,
     PRIMARY KEY (id_services, id_fournisseur)
     );
    
