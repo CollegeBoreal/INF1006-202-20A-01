507ebf8ea8db0eef7c4a87420abee2183c927451
morti747 <54910700+morti747@users.noreply.github.com>
2020-11-03 19:35:35 -0500

    Update etudiant-schema.sql

--- 3.DDL-DCL/300117314/etudiant-schema.sql
+++ 3.DDL-DCL/300117314/etudiant-schema.sql
@@ -1,1 +1,29 @@
 
+-- Comment créer une DATABASE appellée MORTIMAINTENANCE:
+
+mysql> CREATE DATABASE MORTIMAINTENANCE;
+Query OK, 1 row affected (0.09 sec)
+
+-- comment vérifier qu'elle a été crée:
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| MORTIMAINTENANCE   |
+| etudiants          |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+6 rows in set (0.00 sec)
+
+
+
+
+
+
+
+
+
