59fdc72930c32217e28c842a09c6cd1ff8f2355e
Raissa111 <56364697+Raissa111@users.noreply.github.com>
2020-11-08 22:12:28 -0500

    Update 300117782-schema.sql

--- 3.DDL-DCL/300117782-schema.sql
+++ 3.DDL-DCL/300117782-schema.sql
@@ -1,4 +1,11 @@
--- Ma Database
+-- MON DATABASE
+CREATE DATABASE Restaurant_Mamamiya;
 
-CREATE DATABASE  Restaurant_Mamamiya;
+-- CRÉER L'UTILISATEUR (Erna) ET SON MOT DE PASSE (password)
+CREATE USER 'Erna'@'%' IDENTIFIED BY 'password';
 
+-- DONNER TOUT LES PRIVILÈGES À L'UTILISATEUR Erna
+GRANT ALL ON Restaurant_Mamamiya.* TO 'Erna'@'%';
+
+-- ACCÉDER À MA DATABASE 
+USE Restaurant_Mamamiya;
