612f2839ef4c44987b221be8ae7076d632caf5e1
zoureni <56364857+zoureni@users.noreply.github.com>
2020-11-10 11:34:30 -0500

    Update 300118524-schema.sql

--- 3.DDL-DCL/300118524/300118524-schema.sql
+++ 3.DDL-DCL/300118524/300118524-schema.sql
@@ -10,21 +10,41 @@ GRANT ALL ON ALL_TELECOM.* TO 'zoureni'@'%';
 -- ACCÉDER À MON DATABASE 300118524
 USE ALL_TELECOM;
 
+-- CRÉER TABLE FOURNISSEURS
+CREATE TABLE FOURNISSEURS (
+    fournisseur INT AUTO_INCREMENT,
+    nom VARCHAR(24) NOT NULL,
+    telephone INT NOT NULL,
+    addresse VARCHAR(50),
+    PRIMARY KEY(fournisseur)
+    );
+
 -- CRÉER TABLE CLIENT
-CREATE TABLE client (
-    id_client VARCHAR(15),
-    nom_client VARCHAR(24),
-    address_client VARCHAR(50)
+CREATE TABLE CLIENTS (
+    client INT AUTO_INCREMENT,
+    nom VARCHAR(24) NOT NULL,
+    addresse VARCHAR(50),
+    telephone INT NOT NULL,
+    fournisseur INT,
+    PRIMARY KEY(client),
+    FOREIGN KEY(fournisseur) 
+       REFERENCES FOURNISSEURS(fournisseur)
     );
     
--- CRÉER TABLE FOURNISSEURS
-CREATE TABLE fournisseurs (
-    id_fournisseurs VARCHAR(15),
-    nom_fournisseurs VARCHAR(24),
-    address_fournisseurs VARCHAR(50)
-    );
 -- CRÉER TABLE CONNEXION
-CREATE TABLE connexion (
-    connexion_cable BOOLEAN,
-    connexion_DSL BOOLEAN
+CREATE TABLE CONNEXIONS (
+    cable BOOLEAN DEFAULT true,
+    DSL VARCHAR(9),
+    PRIMARY KEY(cable)
+    );
+-- CRÉER TABLE VITESSE
+CREATE TABLE VITESSE (
+    client INT,
+    cable BOOLEAN,
+    vitesse VARCHAR(9),
+    PRIMARY KEY(client, cable),
+    FOREIGN KEY(client) 
+       REFERENCES CLIENTS(client),
+    FOREIGN KEY(cable) 
+       REFERENCES CONNEXIONS(cable)
     );
