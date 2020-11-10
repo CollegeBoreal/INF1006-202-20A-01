a94060536767caabf0b858981fabffd58e83e097
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2020-11-10 02:14:40 -0500

    Update 300116670-schema.sql

--- 3.DDL-DCL/300116670/300116670-schema.sql
+++ 3.DDL-DCL/300116670/300116670-schema.sql
@@ -1,9 +1,28 @@
-CREATE DATABASE MOTOSPORT;
+-- *********************************
+-- Création de ma DATABASE
+-- *********************************
+CREATE IF NO EXISTS DATABASE MOTOSPORT;
 
-USE fournisseurs_et_clients;
 
--- ***********************
+-- *********************************
+-- Creation des utilisateurs et don-
+-- ner les autorisations
+-- *********************************
+CREATE USER 'Auriane'@'%' IDENTIFIED BY 'password';
+GRANT ALL ON MOTOSPORT.* TO 'Auriane'@'%';
+
+
+-- *********************************
+-- CREATION DES TABLES
+-- *********************************
+
 -- Ma table fournisseurs
--- ***********************
-CREATE TABLE fournisseurs (
-   
+CREATE TABLE FOURNISSEURS
+fournisseur INT AUTO_INCREMENT,
+Nom VARCHAR(35) NOT NULL,
+Coordonnees  VARCHAR(13) NOT NULL,
+Addresse VARCHAR(39) NOT NULL;
+
+-- Ma table Services
+CREATE TABLE SERVICES
+
