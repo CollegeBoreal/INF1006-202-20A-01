bc46c7127ae4740ece359f7b11072732e3616a95
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2020-11-10 19:02:37 -0500

    :smile:

--- 3.DDL-DCL/300116973/300116973-schema.sql
+++ 3.DDL-DCL/300116973/300116973-schema.sql
@@ -1,4 +1,4 @@
--- creation de la base de donnee superette_2s_plus
+-- creation de la base de donnee superette_2S_plus
 
 CREATE DATABASE superette_2S_plus;
 
@@ -7,118 +7,117 @@ CREATE DATABASE superette_2S_plus;
 USE superette_2S_plus;
 
 -- creation de l'utilisateur
-CREATE USER 'pascaline'@'%' IDENTIFIED BY 'password';
+CREATE USER 'admin'@'%' IDENTIFIED BY 'password';
 
 -- donnee tout les privileges a l'ulilisateur pascaline
 
-GRANT ALL ON superette_2S_plus.* TO 'pascaline'@'%';
+GRANT ALL ON superette_2S_plus.* TO 'admin'@'%';
 
--- creation de la table fournisseur
+-- creation de la table fournisseurs
 
-CREATE TABLE fournisseur (
-Id_fournisseur INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE FOURNISSEURS(
+fournisseur INT AUTO_INCREMENT NOT NULL,
 Nom_du_fournisseur VARCHAR(25) NOT NULL,
 Numero_de_telephone INT(10) NOT NULL,
 Email VARCHAR(25) NOT NULL,
 Adresse VARCHAR(25) NULL,
-PRIMARY KEY(Id_fournisseur)
+PRIMARY KEY(fournisseur)
 );
 
--- creation de la table produit
+-- creation de la table produits
 
-CREATE TABLE produit (
-Id_produit INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE PRODUITS(
+produit INT AUTO_INCREMENT NOT NULL,
 Nom_du_produit VARCHAR(25) NOT NULL,
 Numero_de_reference VARCHAR(25) NOT NULL,
 Date_de_peremption DATE DEFAULT(1990-01-01) NULL,
-Id_fournisseur INT,
-PRIMARY KEY (Id_produit),
-FOREIGN KEY (Id_fournisseur) REFERENCES fournisseur
- (Id_fournisseur)
+fournisseur INT,
+PRIMARY KEY (produit),
+FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS
+(fournisseur)
  );
 
--- creation de la table commande
+-- creation de la table commandes
 
-CREATE TABLE commande (
-Id_commande INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE COMMANDES(
+commande INT AUTO_INCREMENT NOT NULL,
 Date_de_commande DATE DEFAULT (1990-01-01) NULL,
 Date_et_heure_de_livraison DATETIME NOT NULL,
-Id_produit INT,
-PRIMARY KEY (Id_commande),
-FOREIGN KEY (Id_produit) REFERENCES produit (Id_produit)
+quantite INT NOT NULL,
+produit INT,
+PRIMARY KEY (commande, produit),
+FOREIGN KEY (produit) REFERENCES PRODUITS (produit)
 ); 
 
 
--- creation de la table client
+-- creation de la table clients
 
-CREATE TABLE client (
-Id_client INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE CLIENTS(
+client INT AUTO_INCREMENT NOT NULL,
 Nom TEXT(25) NOT NULL,
 Numero_de_telephone INT(10) NOT NULL,
 Email VARCHAR(25) NULL,
-Id_commande INT,
-PRIMARY KEY (Id_client),
-FOREIGN KEY (Id_commande) REFERENCES commande (Id_commande)
+commande INT,
+PRIMARY KEY (client, commande),
+FOREIGN KEY (commande) REFERENCES COMMANDES 
+(commande)
 );
 
--- creation de la table achat
+-- creation de la table achats
 
-CREATE TABLE achat (
-Id_achat INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE ACHATS(
+achat INT AUTO_INCREMENT NOT NULL,
 Montant INT(255) NULL,
 Quantites INT(100) NULL,
 Date_dachat DATE DEFAULT (1990-01-01) NULL,
-Id_fournisseur INT,
-Id_produit INT,
-PRIMARY KEY (Id_achat),
-FOREIGN KEY (Id_fournisseur) REFERENCEs fournisseur 
-(Id_fournisseur),
-FOREIGN KEY (Id_produit) REFERENCES produit (Id_produit)
+fournisseur INT,
+produit INT,
+PRIMARY KEY (achat, produit),
+FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS 
+(fournisseur),
+FOREIGN KEY (produit) REFERENCES PRODUITS (produit)
 );
 
 
--- creation de la table facture
+-- creation de la table factures
 
-CREATE TABLE facture(
-Id_facture INT AUTO_INCREMENT NOT NULL,
-Montant INT(255) NULL,
+CREATE TABLE FACTURES(
+facture INT AUTO_INCREMENT NOT NULL,
+Montant INT(255) NOT NULL,
 Quantites INT(100) NULL,
 Date_facturation DATE DEFAULT (1990-01-01) NULL,
-Id_client INT,
-Id_commande INT,
-PRIMARY KEY (Id_facture),
-FOREIGN KEY (Id_client) REFERENCES client (Id_client),
-FOREIGN KEY (Id_commande) REFERENCES commande (Id_commande)
+client INT,
+PRIMARY KEY (facture, client),
+FOREIGN KEY (client) REFERENCES CLIENTS (client)
 );
 
+-- creation de la table employes
 
--- creation de la table employe
-
-CREATE TABLE employe(
+CREATE TABLE EMPLOYES(
 Matricule INT AUTO_INCREMENT NOT NULL,
 Nom VARCHAR(25) NOT NULL,
 prenom VARCHAR(25) NULL,
 Numero_de_telephone INT(10) NOT NULL,
 Sexe_feminin BOOLEAN DEFAULT (TRUE OR FALSE) NULL,
-Id_achat INT,
-Id_fournisseur INT,
-PRIMARY KEY (Matricule),
-FOREIGN KEY (Id_achat) REFERENCES achat (Id_achat),
-FOREIGN KEY (Id_fournisseur) REFERENCES fournisseur
-(Id_fournisseur)
+achat INT,
+fournisseur INT,
+PRIMARY KEY (Matricule, achat, fournisseur),
+FOREIGN KEY (achat) REFERENCES ACHATS (achat),
+FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS
+(fournisseur)
 );
 
 
--- creation de la table paiement
+-- creation de la table paiements
 
-CREATE TABLE paiement(
-Id_paiement INT NOT NULL,
+CREATE TABLE PAIEMENTS(
+paiement INT NOT NULL,
 Montant FLOAT(250,00) NULL,
-Id_client INT,
-Id_facture INT,
-Id_commande INT,
-FOREIGN KEY (Id_client) REFERENCES client (Id_client),
-FOREIGN KEY (Id_facture) REFERENCES facture (Id_facture),
-FOREIGN KEY (Id_commande) REFERENCES commande (Id_commande)
+client INT,
+facture INT,
+commande INT,
+PRIMARY KEY (client, facture, commande),
+FOREIGN KEY (client) REFERENCES CLIENTS (client),
+FOREIGN KEY (facture) REFERENCES FACTURES (facture),
+FOREIGN KEY (commande) REFERENCES COMMANDES (commande)
 );
-
