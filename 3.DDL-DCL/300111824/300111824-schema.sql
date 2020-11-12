04be5804fe2fa75a04911fa4139515f0fce097ff
300111824 <54911706+300111824@users.noreply.github.com>
2020-11-12 18:29:50 -0500

    Update 300111824-schema.sql

--- 3.DDL-DCL/300111824/300111824-schema.sql
+++ 3.DDL-DCL/300111824/300111824-schema.sql
@@ -1,1 +1,48 @@
 
+-- Etape de créer la base de données
+
+
+CREATE DATABASE IF NOT EXISTS AGRICOLE
+
+CREATE USER IF NOT EXISTS 'blevie'@'%' IDENTIFIED BY 'password';
+GRANT ALL ON AGRICOLE.* TO 'blevie'@'%';
+
+USE AGRICOLE;
+
+
+-- Les tables
+
+CREATE TABLE FOURNISSEURS (
+  fournisseur INT AUTO_INCREMENT,
+  Nom VARCHAR(10) NOT NULL,
+  Adresse VARCHAR(30),
+  Telephone INT,
+  PRIMNARY KEY(fournisseur)
+  );
+  
+  
+ CREATE TABLE CLIENTS (
+   client INT AUTO_INCREMENT,
+   Nom VARCHAR(10) NOT NULL,
+   Prenom VARCHAR(10),
+   Adresse VARCHAR(30),
+   Telephone INT NOT NULL,
+   PRIMARY KEY(client)
+   );
+   
+   
+   CREATE TABLE PRODUITS (
+     produit INT AUTO_INCREMENT,
+     Nom VARCHAR(10),
+     Client INT,
+     Fournisseur VARCHAR(10),
+     PRIMARY KEY(produit)
+     );
+     
+     
+  
+
+
+
+
+
