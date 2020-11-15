76e68f25089d8c53a3bbaf897b68c4d2993cf39b
morti747 <54910700+morti747@users.noreply.github.com>
2020-11-15 17:08:11 -0500

    Update etudiant-schema.sql

--- 3.DDL-DCL/300117314/etudiant-schema.sql
+++ 3.DDL-DCL/300117314/etudiant-schema.sql
@@ -49,70 +49,64 @@ mysql>
 
 --Comment créer une nouvelle table aves des différents chambres:
 
-
-mysql> CREATE TABLE Instruments
-    -> (Instrument_id INT AUTO_INCREMENT,
-    -> nom VARCHAR(40) NOT NULL,
-    -> MAC_Address VARCHAR(30) NOT NULL,
-    -> date_de_achat DATE NOT NULL,
-    -> vendeur VARCHAR(20),
-    -> commentaire VARCHAR(100),
-    -> PRIMARY KEY (Instrument_id)
-    ->  );
-uery OK, 0 rows affected (0.54 sec)
-
-
---pour vérifier notre nouvelle table:
-
-mysql> DESCRIBE Instruments;
-+---------------+--------------+------+-----+---------+----------------+
-| Field         | Type         | Null | Key | Default | Extra          |
-+---------------+--------------+------+-----+---------+----------------+
-| Instrument_id | int          | NO   | PRI | NULL    | auto_increment |
-| nom           | varchar(40)  | NO   |     | NULL    |                |
-| MAC_Address   | varchar(30)  | NO   |     | NULL    |                |
-| date_de_achat | date         | NO   |     | NULL    |                |
-| vendeur       | varchar(20)  | YES  |     | NULL    |                |
-| commentaire   | varchar(100) | YES  |     | NULL    |                |
-+---------------+--------------+------+-----+---------+----------------+
-6 rows in set (0.00 sec)
+CREATE TABLE Instruments
+(Instrument INT AUTO_INCREMENT,
+nom VARCHAR(40) NOT NULL,
+vendeur VARCHAR(20),
+Address_physique VARCHAR(30) NOT NULL,
+date_de_reception DATE NOT NULL,
+date_de_livraison DATE NOT NULL,
+commentaire VARCHAR(100),
+PRIMARY KEY (Instrument)
+);
 
 
 
-
---Créer d'autre table: 
+--Créer la table client: 
 
 mysql> CREATE TABLE Clients
-    -> (client_id INT AUTO_INCREMENT,
+    -> (client INT AUTO_INCREMENT,
     -> prenom VARCHAR(20) NOT NULL,
     -> nom VARCHAR(20) NOT NULL,
     -> adresse_email VARCHAR(20) NOT NULL,
     -> telephone INT NOT NULL,
     -> adresse VARCHAR(20),
     -> nom_de_entreprise VARCHAR(20),
-    -> PRIMARY KEY(client_id)
+    -> PRIMARY KEY(client)
     -> );
-Query OK, 0 rows affected (0.30 sec)
 
 
+--Créer la table employé: 
+
+CREATE TABLE employes
+(employe INT AUTO_INCREMENT,
+prenom VARCHAR(20) NOT NULL,
+nom VARCHAR(20) NOT NULL,
+adresse_email VARCHAR(20) NOT NULL,
+telephone INT NOT NULL,
+adresse VARCHAR(20),
+nom_de_departement VARCHAR(20),
+PRIMARY KEY(employe)
+)
+
+--Créer la table département:
+
+CREATE TABLE Departement
+(departement INT AUTO_INCREMENT,
+nom VARCHAR(20) NOT NULL,
+telephone INT NOT NULL,
+responsible VARCHAR(20),
+PRIMARY KEY(departement)
+);
+
+--Créer la table sérvices:
+
+CREATE TABLE Services
+(service INT AUTO_INCREMENT,
+Instrument
+detail VARCHAR(20)
 
 
---pour vérifier notre nouvelle table:
-
-
-mysql>  DESCRIBE Clients;
-+-------------------+-------------+------+-----+---------+----------------+
-| Field             | Type        | Null | Key | Default | Extra          |
-+-------------------+-------------+------+-----+---------+----------------+
-| client_id         | int         | NO   | PRI | NULL    | auto_increment |
-| prenom            | varchar(20) | NO   |     | NULL    |                |
-| nom               | varchar(20) | NO   |     | NULL    |                |
-| adresse_email     | varchar(20) | NO   |     | NULL    |                |
-| telephone         | int         | NO   |     | NULL    |                |
-| adresse           | varchar(20) | YES  |     | NULL    |                |
-| nom_de_entreprise | varchar(20) | YES  |     | NULL    |                |
-+-------------------+-------------+------+-----+---------+----------------+
-7 rows in set (0.00 sec)
 
 
 
