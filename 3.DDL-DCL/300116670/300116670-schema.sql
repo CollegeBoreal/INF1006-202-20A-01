983671d14b8d0bc199ccd42a5d5eb223891d2886
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2020-11-16 21:09:35 -0500

    Update 300116670-schema.sql

--- 3.DDL-DCL/300116670/300116670-schema.sql
+++ 3.DDL-DCL/300116670/300116670-schema.sql
@@ -25,6 +25,8 @@ Coordonnees VARCHAR(13) NOT NULL,
 Addresse VARCHAR(39) NOT NULL,
 Addresse_electronique VARCHAR(50)
 PRIMARY KEY (fournisseur)
+FOREIGN KEY (produit) REFERENCES PRODUITS(produit)
+
 );
 
 -- Ma table CLIENTS
@@ -40,7 +42,7 @@ PRIMARY KEY (client)
 
 -- Ma table VENTES
 CREATE TABLE VENTES(
-vente INT AUTO_INCREMNT,
+vente INT AUTO_INCREMENT,
 Nom_du_produit VARCHAR(40) NOT NULL,
 Date_et_heure_de_la_vente DATETIME NOT NULL, 
 Date_et_heure_de_livraison DATETIME NOT NULL,
@@ -56,22 +58,29 @@ Nom_du_produit VARCHAR(20),
 Numero_de_reference VARCHAR(30),
 Prix_du_produit INT NOT NULL,
 Type_du_produit VARCHAR(15)
-PRIMARY KEY (produit)
+PRIMARY KEY (produit),
+FOREIGN KEY (location) REFERENCES LOCATIONS(location),
+FOREIGN KEY (vente) REFERENCES VENTES(vente),
+FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS(fournisseur)
 );
 
 -- Ma table LOCATIONS
 CREATE TABLE LOCATIONS(
 location INT AUTO_INCREMENT,
-location_standard,
-location_premium,
-prix_location_standard,
-prix_location_premium,
+type_de_location
 date_de_sortie,
 date_de_retour,
 PRIMARY KEY (location)
+FOREIGN KEY (produit) REFERENCES PRODUITS(produit),
+FOREIGN KEY (vente) REFERENCES VENTES(vente),
+FOREIGN KEY (client) REFERENCES CLIENTS(client)
 );
-
 
 -- Ma table SERVICES_LOCATION
 CREATE TABLE SERVICES_LOCATION(
-
+service_location INT,
+type_de_location VARCHAR(20) NOT NULL,
+prix_de_la_location INT NOT NULL,
+date_de_sortie DATETIME,
+date_de_retour DATETIME
+);
