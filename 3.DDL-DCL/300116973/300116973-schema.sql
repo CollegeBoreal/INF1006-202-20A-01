19ba2e85544cc942723f8e051c54b5eca005c7a0
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2020-11-17 08:59:01 -0500

    :bug:

--- 3.DDL-DCL/300116973/300116973-schema.sql
+++ 3.DDL-DCL/300116973/300116973-schema.sql
@@ -15,11 +15,11 @@ GRANT ALL ON superette_2S_plus.* TO 'admin'@'%';
 
 -- creation de la table fournisseurs
 
-CREATE TABLE FOURNISSEURS(
-fournisseur INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE FOURNISSEURS (
+fournisseur INT AUTO_INCREMENT,
 Nom_du_fournisseur VARCHAR(25) NOT NULL,
-Numero_de_telephone INT(10) NOT NULL,
-Email VARCHAR(25) NOT NULL,
+Numero_de_telephone INT NOT NULL,
+Email VARCHAR(25) NULL,
 Adresse VARCHAR(25) NULL,
 PRIMARY KEY(fournisseur)
 );
@@ -27,12 +27,12 @@ PRIMARY KEY(fournisseur)
 -- creation de la table produits
 
 CREATE TABLE PRODUITS(
-produit INT AUTO_INCREMENT NOT NULL,
+produit INT AUTO_INCREMENT,
 Nom_du_produit VARCHAR(25) NOT NULL,
-Numero_de_reference VARCHAR(25) NOT NULL,
-Date_de_peremption DATE DEFAULT(1990-01-01) NULL,
+Numero_de_reference VARCHAR(25),
+Date_de_peremption DATE DEFAULT '1990-01-01' NULL,
 fournisseur INT,
-PRIMARY KEY (produit),
+PRIMARY KEY (produit, fournisseur),
 FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS
 (fournisseur)
  );
@@ -40,9 +40,9 @@ FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS
 -- creation de la table commandes
 
 CREATE TABLE COMMANDES(
-commande INT AUTO_INCREMENT NOT NULL,
-Date_de_commande DATE DEFAULT (1990-01-01) NULL,
-Date_et_heure_de_livraison DATETIME NOT NULL,
+commande INT AUTO_INCREMENT,
+Date_de_commande DATE DEFAULT '1990-01-01' NULL,
+Date_et_heure_de_livraison DATETIME,
 quantite INT NOT NULL,
 produit INT,
 PRIMARY KEY (commande, produit),
@@ -53,9 +53,9 @@ FOREIGN KEY (produit) REFERENCES PRODUITS (produit)
 -- creation de la table clients
 
 CREATE TABLE CLIENTS(
-client INT AUTO_INCREMENT NOT NULL,
+client INT AUTO_INCREMENT,
 Nom TEXT(25) NOT NULL,
-Numero_de_telephone INT(10) NOT NULL,
+Numero_de_telephone INT NOT NULL,
 Email VARCHAR(25) NULL,
 commande INT,
 PRIMARY KEY (client, commande),
@@ -66,15 +66,12 @@ FOREIGN KEY (commande) REFERENCES COMMANDES
 -- creation de la table achats
 
 CREATE TABLE ACHATS(
-achat INT AUTO_INCREMENT NOT NULL,
-Montant INT(255) NULL,
-Quantites INT(100) NULL,
-Date_dachat DATE DEFAULT (1990-01-01) NULL,
-fournisseur INT,
+achat INT AUTO_INCREMENT,
+Montant INT NOT NULL,
+Quantites INT NOT NULL,
+Date_dachat DATE DEFAULT '1990-01-01',
 produit INT,
 PRIMARY KEY (achat, produit),
-FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS 
-(fournisseur),
 FOREIGN KEY (produit) REFERENCES PRODUITS (produit)
 );
 
@@ -83,41 +80,34 @@ FOREIGN KEY (produit) REFERENCES PRODUITS (produit)
 
 CREATE TABLE FACTURES(
 facture INT AUTO_INCREMENT NOT NULL,
-Montant INT(255) NOT NULL,
-Quantites INT(100) NULL,
-Date_facturation DATE DEFAULT (1990-01-01) NULL,
-client INT,
-PRIMARY KEY (facture, client),
-FOREIGN KEY (client) REFERENCES CLIENTS (client)
+Montant INT NOT NULL,
+Quantites INT NULL,
+Date_facturation DATE DEFAULT '1990-01-01' NULL,
+PRIMARY KEY (facture)
 );
 
 -- creation de la table employes
 
 CREATE TABLE EMPLOYES(
-Matricule INT AUTO_INCREMENT NOT NULL,
+Matricule INT AUTO_INCREMENT,
 Nom VARCHAR(25) NOT NULL,
 prenom VARCHAR(25) NULL,
-Numero_de_telephone INT(10) NOT NULL,
-Sexe_feminin BOOLEAN DEFAULT (TRUE OR FALSE) NULL,
+Numero_de_telephone INT NOT NULL,
+Sexe_feminin BOOLEAN DEFAULT TRUE NULL,
 achat INT,
-fournisseur INT,
-PRIMARY KEY (Matricule, achat, fournisseur),
-FOREIGN KEY (achat) REFERENCES ACHATS (achat),
-FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS
-(fournisseur)
+PRIMARY KEY (Matricule),
+FOREIGN KEY (achat) REFERENCES ACHATS (achat)
 );
 
 
 -- creation de la table paiements
 
 CREATE TABLE PAIEMENTS(
-paiement INT NOT NULL,
-Montant FLOAT(250,00) NULL,
-client INT,
-facture INT,
+paiement INT AUTO_INCREMENT,
+Montant FLOAT NULL,
 commande INT,
-PRIMARY KEY (client, facture, commande),
-FOREIGN KEY (client) REFERENCES CLIENTS (client),
-FOREIGN KEY (facture) REFERENCES FACTURES (facture),
-FOREIGN KEY (commande) REFERENCES COMMANDES (commande)
+facture INT,
+PRIMARY KEY (paiement, commande, facture),
+FOREIGN KEY (commande) REFERENCES COMMANDES (commande),
+FOREIGN KEY (facture) REFERENCES FACTURES (facture)
 );
