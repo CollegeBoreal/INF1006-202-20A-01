c60d53b7906c7524023a176c8f8b3cfac27b092b
amirali175 <54910751+amirali175@users.noreply.github.com>
2020-11-20 14:56:25 -0500

    Update 300116685-schema.sql

--- 3.DDL-DCL/300116685/300116685-schema.sql
+++ 3.DDL-DCL/300116685/300116685-schema.sql
@@ -1,20 +1,28 @@
 ==============================================
- --               DJIB-12
+ --   ma base de donnees DJIB_12
+ CREATE DATABASE DJIB_12;
+ USE DJIB_12;
+ -- creation d'utilisateur
+ CREATE USER 'amirali11'@'%' IDENTIFIED BY 'Password';
+ -- donne tous les privilegies utilisateur a amirali11
+ GRANT ALL ON DJIB_12.* TO 'amirali11'@'%';
 ==============================================
 --Table employe 
 ===============================================
-CREATE TABLE 'table_employe'
+
+CREATE TABLE TABLES_EMPLOYEES 
 (
-  `idTable_employee` int(11) NOT NULL,
-  `Prenom` varchar(45) NOT NULL,
-  `Nom` varchar(45) NOT NULL,
-  `Poste` varchar(45) NOT NULL,
-  `Bureau` varchar(45) NOT NULL,
-  `Salaire` varchar(45) NOT NULL,
-  `Commission` varchar(45) DEFAULT NULL,
-  `Embauche` varchar(45) DEFAULT NULL,
-  `table_employeecol` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idTable_employee`)
+  Table_employee int AUTO_INCREMENT ,
+  Prenom varchar(45) NOT NULL,
+  Nom varchar(45) NOT NULL,
+  Poste varchar(45)  NULL,
+  Bureau varchar(45)  NULL,
+  Salaire varchar(45) NOT NULL,
+  Commission varchar(45) DEFAULT NULL,
+  Embauche varchar(45) DEFAULT NULL,
+  table_employeecol varchar(45) DEFAULT NULL,
+  PRIMARY KEY (Table_employee)
+  );
 ============================================================
 --Table fournisseur 
 =============================================================
