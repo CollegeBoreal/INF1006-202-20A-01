a95332df65b20301f5c9bc834ed17a5539d9c816
amirali175 <54910751+amirali175@users.noreply.github.com>
2020-11-22 11:46:20 -0500

    :horse:

---
+++ 3.DDL-DCL/300116685/300116685-data.sql
@@ -0,0 +1,3 @@
+-- Les donnees de table Employee
+INSERT INTO `DJIB_12`.`TABLES_EMPLOYEES` (`Table_employee`, `Prenom`, `Nom`, `Poste`, `Bureau`, `Salaire`, `Commission`, `Embauche`) 
+VALUES ('1', 'Bahar ', 'Ai ', 'Vendeur', 'Toronto', '4500', '0', '1992-01-02');
