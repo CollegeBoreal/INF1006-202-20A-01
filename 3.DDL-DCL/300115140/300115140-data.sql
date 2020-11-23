e0fc29ba112eec76945eeb5f001f0083c3121625
Zack <54910329+ZackTO@users.noreply.github.com>
2020-11-23 11:09:49 -0500

    Update 300115140-data.sql

--- 3.DDL-DCL/300115140/300115140-data.sql
+++ 3.DDL-DCL/300115140/300115140-data.sql
@@ -1,1 +1,62 @@
 
+--La table FOURNISSEURS
+
+INSERT INTO FOURNSSEURS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO FOURNSSEURS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO FOURNSSEURS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO FOURNSSEURS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO FOURNSSEURS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+
+
+
+-- La table CLIENTS
+
+INSERT INTO CLIENTS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO CLIENTS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO CLIENTS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO CLIENTS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO CLIENTS (Nom, Adresse, Telephone) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+
+
+-- La TABLE PRODUITS
+
+INSERT INTO PRODUITS (Nom_du_produit, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO PRODUITS (Nom_du_produit, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO PRODUITS (Nom_du_produit, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO PRODUITS (Nom_du_produit, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO PRODUITS (Nom_du_produit, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO PRODUITS (Nom_du_produit, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO PRODUITS (Nom_du_produit, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+
+
+-- La table SERVICES
+
+
+INSERT INTO SERVICES (Nom_du_service, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO SERVICES (Nom_du_service, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO SERVICES (Nom_du_service, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO SERVICES (Nom_du_service, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO SERVICES (Nom_du_service, fournisseur) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+
+
+-- La table VENTES_DE_SERV
+
+
+INSERT INTO VENTES_DE_SERV (Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_SERV (Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_SERV (Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_SERV (Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_SERV (Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_SERV (Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+
+
+-- La table VENTES_DE_PROD
+
+INSERT INTO VENTES_DE_PROD (client, Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_PROD (client, Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_PROD (client, Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_PROD (client, Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+INSERT INTO VENTES_DE_PROD (client, Prix, Date_de_vente) VALUES ('BIOCAMP', '3 rue Legrand', 33401234 )
+
+
+
