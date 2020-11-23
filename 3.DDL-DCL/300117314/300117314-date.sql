3bc88d23ae2f79c2a1201bdaa09df8288c3fb3a9
morti747 <54910700+morti747@users.noreply.github.com>
2020-11-23 12:32:37 -0500

    Update 300117314-date.sql

--- 3.DDL-DCL/300117314/300117314-date.sql
+++ 3.DDL-DCL/300117314/300117314-date.sql
@@ -1,3 +1,5 @@
+--remplir la table de Clients:
+
 mysql> INSERT INTO Clients (prenom, nom, adresse_email, telephone, adresse, nom_de_entreprise) VALUES ( 'Kala', 'Wiliam', 'kala.wiliam.com', 64725625, '24-Tudor-St', 'Rogers');
 mysql> INSERT INTO Clients (prenom, nom, adresse_email, telephone, adresse, nom_de_entreprise) VALUES ( 'Philip', 'Cramer', 'philip.cramer.com', 64725622, '15 Poplar St', "La maison d'asie");
 mysql> INSERT INTO Clients (prenom, nom, adresse_email, telephone, adresse, nom_de_entreprise) VALUES ( 'John', 'Steel', 'john.steel.com', 64725178, '158 Cedar crs', "La maison d'asie");
@@ -11,6 +13,17 @@ mysql> INSERT INTO Clients (prenom, nom, adresse_email, telephone, adresse, nom_
 
 
 
+
+--remplir la table de Employes:
+
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Richard', 'Anderson', 'richard.anderson.com', 51425983, '48 Garden Ave', "Materiel");
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Ema', 'Limeira', 'ema.limeira.com', 51424979, '68 Fifeshire Ave', "Materiel");
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Michael', 'Holze', 'michael.holz.com', 51424922, '226 Newton Dr', "Materiel");
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Maurizio', 'Moroni', 'morizio.moroni.com', 51424221, '23 Blackforest Ave', "Materiel");
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Isabel', 'Sandra', 'isabel.sandra.com', 51424295, '170 Princess Ave', "logiciel");
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Jean', 'Fresnier', 'jean.fresnier.com', 51424353, '87 Goremley Rd', "logiciel");
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Francois', 'Rovelli', 'francois.rovelli.com', 51424773, '121 Shudell Ave', "logiciel");
+mysql> INSERT INTO Employes (prenom, nom, adresse_email, telephone, adresse, nom_de_departement) VALUES ( 'Howard', 'Synder', 'howard.synder.com', 51424723, '189 Churchill Ave', "logiciel");
 
 
 
