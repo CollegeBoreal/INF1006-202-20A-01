4f9c49b32db2b979bc00b9edae6f5d173005704f
Idir <54910261+dido05327@users.noreply.github.com>
2020-11-23 19:33:36 -0500

    300117444.sql

--- 2.DQL/300117444/300117444.sql
+++ 2.DQL/300117444/300117444.sql
@@ -1,56 +1,54 @@
---Marequete sql
-SELECT * FROM sakila.actor WHERE first_name = 'NICK' ;
 -- Imprimer le nom et le montant de la facture totale des personnes habitant la ville de Lubumbashi
-SELECT city.city, customer.first_name, SUM(payment.amount)
-FROM city
+
+SELECT city,customer.last_name, SUM(payment.amount) FROM city
 JOIN address USING (city_id)
 JOIN customer USING (address_id)
 JOIN payment USING (customer_id)
-WHERE city = "Lubumbashi"
-GROUP BY city.city, customer.first_name;
--- Imprimer le nom et le montant de factures des personnes habitant le Congo ou l'algerie (ANSI-92)
-SELECT country.country, customer.first_name, payment.amount
-FROM country 
+WHERE city='lubumbashi'
+GROUP BY city, customer.last_name;
+
+
+-- Imprimer le nom et le montant de factures des personnes habitant le Congo et l'algerie (ANSI-92)
+
+SELECT country, customer.last_name, payment.amount FROM country
 JOIN city USING (country_id)
 JOIN address USING (city_id)
 JOIN customer USING (address_id)
-JOIN payment USING ( customer_id)
-WHERE country LIKE "congo%" OR country = "algeria"
-;
--- Imprimer le nom et le montant de la facture totale des personnes habitant le Congo ou l'Algerie (ANSI-92)
-SELECT country.country, customer.first_name, SUM(payment.amount)
-FROM country 
+JOIN payment USING (customer_id)
+WHERE country like "Congo%" OR "Algeri%";
+
+
+
+
+--Imprimer le nom et le montant de la facture totale des personnes habitant le Congo ou l'Algerie (ANSI-92)
+
+
+SELECT country, customer.last_name, SUM(payment.amount) FROM country
 JOIN city USING (country_id)
 JOIN address USING (city_id)
 JOIN customer USING (address_id)
-JOIN payment USING ( customer_id)
-WHERE country LIKE "congo%" OR country = "algeria"
-GROUP BY country.country, customer.first_name;
-;
--- Quel(le) acteur(rice) est apparu(e) dans le plus de films?
-SELECT actor.first_name, COUNT(*)
-FROM film 
+JOIN payment USING (customer_id)
+WHERE country like "Congo%" OR "Algeri%"
+GROUP BY country, customer.last_name;
+
+--Quel(le) acteur(rice) est apparu(e) dans le plus de films?
+
+SELECT actor.last_name, actor_id, COUNT(*) FROM film
 JOIN film_actor USING (film_id)
-JOIN actor USING (actor_id)
+JOIN actor USING  (actor_id)
+GROUP BY actor.last_name, actor_id LIMIT 1;
 
-GROUP BY actor.first_name LIMIT 1;
+--Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne
 
-
-
---Quel est la longueur moyenne des films par catégorie? Trié par longueur moyenne 
-SELECT category.name, AVG(film.length)
-FROM film
+SELECT category.name , AVG(film.length) FROM film
 JOIN film_category USING (film_id)
 JOIN category USING (category_id)
-GROUP BY category.name; 
-
-
-
-
+GROUP BY category.name;
 
 -- Quel sont les catégories qui ont les plus longs films (i.e. dépassant la longueur moyenne) ? Trié par longueur moyenne
-SELECT category.name, COUNT(length) > AVG(film.length)
-FROM film
+
+SELECT category.name , AVG(film.length) FROM film
 JOIN film_category USING (film_id)
 JOIN category USING (category_id)
-GROUP BY category.name; 
+GROUP BY category.name HAVING AVG(length) > (SELECT AVG(film.length) FROM film) ;
+
