8cf5447fef44e211b0310bf063046fdf6f60d801
Raissa111 <56364697+Raissa111@users.noreply.github.com>
2020-11-24 21:29:59 -0500

    Update 300117782-schema.sql

--- 3.DDL-DCL/300117782/300117782-schema.sql
+++ 3.DDL-DCL/300117782/300117782-schema.sql
@@ -10,7 +10,7 @@ GRANT ALL ON Restaurant_Mamamiya.* TO 'Erna'@'%';
 -- ACCÉDER À MA DATABASE 
 USE Restaurant_Mamamiya;
 
--- Creation de la table plats
+-- Creation de la table plat
 
 CREATE TABLE PLATS (
 plat INT AUTO_INCREMENT NOT NULL,
@@ -19,69 +19,65 @@ Montant INT (40) NULL,
 PRIMARY KEY (plat)
 );
 
--- creation de la table Menu
+-- creation de la table ingredient
 
-CREATE TABLE MENUS (
-menu INT AUTO_INCREMENT NOT NULL,
-Nom_du_menu VARCHAR(25) NOT NULL,
-plat INT,  
-PRIMARY KEY (menu),
-FOREIGN KEY (plat) REFERENCES PLATS (plat)  
+  CREATE TABLE INGREDIENTS (
+  ingredient INT NOT NULL AUTO_INCREMENT,
+  Nom_ingredient VARCHAR(20) NOT NULL,
+  PRIMARY KEY (ingredient)
 );
+
+-- creation de la table recette
+  CREATE TABLE RECETTES (
+  recette INT NOT NULL AUTO_INCREMENT,
+  Nom_du_recette VARCHAR(25) NOT NULL,
+  ingredient INT NOT NULL,
+  PRIMARY KEY (recette, INGREDIENTS_ingredient),
+  FOREIGN KEY (INGREDIENTS_ingredient)
+  );
 
 -- creation de la table commande
 
-CREATE TABLE COMMANDES (
-commande INT AUTO_INCREMENT NOT NULL,
-Date_et_heure_de_commande DATETIME NOT NULL,
-menu INT,
-PRIMARY KEY (commande, menu),
-FOREIGN KEY (menu) REFERENCES MENUS(menu)
-); 
---creation de la table ingredient
-
-CREATE TABLE INGREDIENTS (
-ingredient INT AUTO_INCREMENT NOT NULL,
-Nom_ingredient VARCHAR(20) NOT NULL,
-menu INT,
-plat INT,
-PRIMARY KEY (ingredient, plat, menu),
-FOREIGN KEY (plat) REFERENCES PLATS (plat),
-FOREIGN KEY (menu) REFERENCES MENUS (menu)
-);
-
-  -- creation de la table client
-
-CREATE TABLE CLIENTS (
-client INT AUTO_INCREMENT NOT NULL,
-Nom TEXT(25) NOT NULL,
-commande INT,
-PRIMARY KEY (client, commande),
-FOREIGN KEY (commande) REFERENCES COMMANDES (commande)
-);
+  CREATE TABLE COMMANDES (
+  commande INT NOT NULL AUTO_INCREMENT,
+  Date_et_heure_de_commande` DATETIME NOT NULL,
+  plat INT NOT NULL,
+  PRIMARY KEY (commande, plat),
+  FOREIGN KEY (plat)
+  );
   
-   -- creation de la table facture
-   
-CREATE TABLE FACTURES(
-facture INT AUTO_INCREMENT NOT NULL,
-Montant INT(100) NULL,
-Quantites INT(20) NULL,
-client INT,
-commande INT,
-PRIMARY KEY (facture, client, commande),
-FOREIGN KEY (client) REFERENCES CLIENTS (client),
-FOREIGN KEY (commande) REFERENCES COMMANDES (commande)
-);
-  -- creation de la table paiement
+   -- creation de la table client
+  
+  CREATE TABLE CLIENTS (
+  client INT NOT NULL AUTO_INCREMENT,
+  Nom TINYTEXT NOT NULL,
+  commande INT NOT NULL,
+  PRIMARY KEY (client, commande),
+  FOREIGN KEY (commande)
+  );
+    
+  -- creation de le table facture
+ 
+  CREATE TABLE FACTURES (
+  facture INT NOT NULL AUTO_INCREMENT,
+  montant INT NULL DEFAULT NULL,
+  quantites INT NULL DEFAULT NULL,
+  PRIMARY KEY (facture)
+  );
 
-CREATE TABLE PAIEMENTS(
-paiement INT NOT NULL,
-Montant FLOAT(200,00) NULL,
-Date_et_heure_de_Paiement DATETIME NOT NULL,
-client INT,
-facture INT,
-commande INT,
-FOREIGN KEY (client) REFERENCES CLIENTS (client),
-FOREIGN KEY (facture) REFERENCES FACTURES (facture)
-);
+  -- creation de la table paiement
+  
+  CREATE TABLE PAIEMENTS (
+  paiement INT NOT NULL,
+  montant FLOAT(200,0) NULL DEFAULT NULL,
+  Date_et_heure_de_Paiement DATETIME NOT NULL,
+  client INT NULL DEFAULT NULL,
+  facture INT NULL DEFAULT NULL,
+  commande INT NULL DEFAULT NULL,
+  FOREIGN KEY (client)
+  FOREIGN KEY (facture)
+  );
+      
+    
+    
 
