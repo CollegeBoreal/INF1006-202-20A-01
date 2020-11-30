dc189347df493f7ffea5b7605bc0c2205ec19e69
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2020-11-30 00:31:23 -0500

    Update 300116670-data.sql

--- 3.DDL-DCL/300116670/300116670-data.sql
+++ 3.DDL-DCL/300116670/300116670-data.sql
@@ -1,1 +1,33 @@
+-- donnees table fournisseurs
+INSERT INTO FOURNISSEURS (Nom, Coordonnees,Adresse) VALUES ('HJC','647-555-6678','1 Yonge street')
+INSERT INTO FOURNISSEURS (Nom, Coordonnees,Adresse) VALUES ('BEL-PAK','647-655-6778','21 Barthust street')
+INSERT INTO FOURNISSEURS (Nom, Coordonnees,Adresse) VALUES ('ROLLMAX','647-666-8978','121 Egglinton street')
+INSERT INTO FOURNISSEURS (Nom, Coordonnees,Adresse) VALUES ('BERTAFREY','647-858-6978','90 Lawrence street')
+
+-- donnees table produits
+
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Quad 4x4',2250,'standard')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Goldwing',27000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Buggy 4x4',1000,'standard')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Quad T3',7000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('SSV',10000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Panigale V4',1150,'standard')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Harley Davidson',45000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Moto trial',3500,'standard')
+
+-- donnees table ventes
+
+INSERT INTO CLIENTS (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('',2250,'standard')
+INSERT INTO CLIENTS (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Goldwing',27000,'premium')
+INSERT INTO CLIENTS (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Buggy 4x4',1000,'standard')
+INSERT INTO CLIENTS (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Quad T3',7000,'premium')
+INSERT INTO CLIENTS (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('SSV',10000,'premium')
+
+-- donnees table clients
+
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Kouta','Auriane','647-648-5657,'22 Garden avenue east')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Batchi','Ezechiel','647-250-8987','123 Baldwin street')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Ngoma','Christopher','647-470-2250','79 Gridwood street') 
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Green','Desmond',647-270-2550,'57 Combermere drive',ngoma)
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Moe','Roger','647-547-5957,'101 Combermere drive')
 
