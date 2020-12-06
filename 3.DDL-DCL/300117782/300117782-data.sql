618cccb6849cb2e815f0500dfb2e5b839c2e1172
Raissa111 <56364697+Raissa111@users.noreply.github.com>
2020-12-06 18:00:42 -0500

    Update 300117782-data.sql

--- 3.DDL-DCL/300117782/300117782-data.sql
+++ 3.DDL-DCL/300117782/300117782-data.sql
@@ -20,12 +20,12 @@ INSERT INTO INGREDIENTS (Nom_ingredient) VALUES ("Mais");
 
 -- Creation des donnees de la table recette
 
-INSERT INTO RECETTES (Nom_de_la_recette, Ingredient) VALUES ("Beignet", 1 );
-INSERT INTO RECETTES (Nom_de_la_recette, Ingredient) VALUES ("Haricot", 2);
-INSERT INTO RECETTES (Nom_de_la_recette, Ingredient) VALUES ("Mais", 7);
+INSERT INTO RECETTES (Nom_de_la_recette, Ingredient, plat) VALUES ("Beignet", 1, 2 );
+INSERT INTO RECETTES (Nom_de_la_recette, Ingredient, plat) VALUES ("Haricot", 2, 1 );
+INSERT INTO RECETTES (Nom_de_la_recette, Ingredient, plat) VALUES ("Mais", 7, 3);
 
 
--- Creation des donnees de la table commande
+-- Creation des donnees de la table commande4
 
 INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-03-02' '05:40:10', 1);
 INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-03-05' '08:10:08', 2);
@@ -51,10 +51,10 @@ INSERT INTO FACTURE(montant, quantites) VALUES (15, 2);
 
 -- Creation des donnees de la table paiement
 
-INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (15, '2020-03-02' '05:40:30', 1, 1,1);
-INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (25, '2020-03-05' '08:10:30', 2, 2,2);
-INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (15, '2020-11-07' '07:20:90', 3, 3,3)
-INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (15, '2020-11-06' '10:350:30', 4, 4,4)
+INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (15, '2020-03-02''05:40:30', 1, 1,1);
+INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (25, '2020-03-05''08:10:30', 2, 2,2);
+INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (15, '2020-11-07''07:20:90', 3, 3,3);
+INSERT INTO PAIEMENTS (montant, Date_et_heure_de_paiement, client, facture, commande) VALUES (15, '2020-11-06''10:350:30', 4, 4,4);
 
 
 
