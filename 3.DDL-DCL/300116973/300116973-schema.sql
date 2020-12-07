40f7bd508d2d8c4a6eb0a12eb1fdbe3d71d41565
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2020-12-07 17:11:13 -0500

    :bug::tomato:

--- 3.DDL-DCL/300116973/300116973-schema.sql
+++ 3.DDL-DCL/300116973/300116973-schema.sql
@@ -33,9 +33,19 @@ Nom_du_produit VARCHAR(25) NOT NULL,
 Numero_de_reference VARCHAR(25),
 Date_de_peremption DATE DEFAULT NULL,
 fournisseur INT,
-PRIMARY KEY (produit, fournisseur),
+PRIMARY KEY (produit),
 FOREIGN KEY (fournisseur) REFERENCES FOURNISSEURS (fournisseur)
  );
+ 
+ -- creation de la table clients
+
+CREATE TABLE CLIENTS(
+client INT AUTO_INCREMENT,
+Nom TEXT(25) NOT NULL,
+Numero_de_telephone INT NOT NULL,
+Email VARCHAR(25) NULL,
+PRIMARY KEY (client)
+);
 
 -- creation de la table commandes
 
@@ -45,23 +55,15 @@ Date_de_commande DATE DEFAULT NULL,
 Date_et_heure_de_livraison DATETIME,
 quantite INT NOT NULL,
 produit INT,
+client INT,
 PRIMARY KEY (commande, produit),
 FOREIGN KEY (produit) REFERENCES PRODUITS (produit),
+FOREIGN KEY (client) REFERENCES CLIENTS 
+(client),
 CONSTRAINT commande_nonzero CHECK (commande <> 0)
 ); 
 
--- creation de la table clients
 
-CREATE TABLE CLIENTS(
-client INT AUTO_INCREMENT,
-Nom TEXT(25) NOT NULL,
-Numero_de_telephone INT NOT NULL,
-Email VARCHAR(25) NULL,
-commande INT,
-PRIMARY KEY (client, commande),
-FOREIGN KEY (commande) REFERENCES COMMANDES 
-(commande)
-);
 
 -- creation de la table achats
 
@@ -75,18 +77,6 @@ PRIMARY KEY (achat, produit),
 FOREIGN KEY (produit) REFERENCES PRODUITS (produit)
 );
 
-
--- creation de la table factures
-
-CREATE TABLE FACTURES(
-facture INT AUTO_INCREMENT NOT NULL,
-Montant INT NOT NULL,
-Quantites INT NULL,
-Date_facturation DATE DEFAULT NULL,
-commande INT,
-PRIMARY KEY (facture, commande),
-FOREIGN KEY (commande) REFERENCES COMMANDES (commande)
-);
 
 -- creation de la table employes
 
@@ -104,6 +94,19 @@ PRIMARY KEY (Matricule),
      ON DELETE CASCADE
 );
 
+-- creation de la table factures
+
+CREATE TABLE FACTURES(
+facture INT AUTO_INCREMENT NOT NULL,
+Montant INT NOT NULL,
+Quantites INT NULL,
+Date_facturation DATE DEFAULT NULL,
+commande INT,
+paiement INT
+PRIMARY KEY (facture, commande, paiement),
+FOREIGN KEY (commande) REFERENCES COMMANDES (commande),
+FOREIGN KEY (paiement) REFERENCES PAIEMENTS (paiement
+);
 
 -- creation de la table paiements
 
