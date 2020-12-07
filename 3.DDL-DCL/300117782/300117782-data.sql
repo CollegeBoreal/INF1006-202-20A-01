66820eef2e2434e10a0e8b19f93b5c06160746f6
Raissa111 <56364697+Raissa111@users.noreply.github.com>
2020-12-07 18:07:20 -0500

    Update 300117782-data.sql

--- 3.DDL-DCL/300117782/300117782-data.sql
+++ 3.DDL-DCL/300117782/300117782-data.sql
@@ -27,10 +27,10 @@ INSERT INTO RECETTES (Nom_de_la_recette, Ingredient, plat) VALUES ("Mais", 7, 3)
 
 -- Creation des donnees de la table commande4
 
-INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-03-02' '05:40:10', 1);
-INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-03-05' '08:10:08', 2);
-INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-11-07' '07:20:90', 4);
-INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-11-06' '10:20:70', 3);
+INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-03-02''05:40:10', 1);
+INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-03-05''08:10:08', 2);
+INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-11-07''07:20:90', 4);
+INSERT INTO COMMANDES (Date_et_heure_de_commande, Plat) VALUES ('2020-11-06''10:20:70', 3);
 
 
 -- Creation des donnees de la table client
