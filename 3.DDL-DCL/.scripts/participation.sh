a35149058d6de3998169978da3279fae9c5acc9e
b300098957 <300098957@collegeboreal.ca>
2020-12-13 00:08:33 -0500

    .

--- 3.DDL-DCL/.scripts/participation.sh
+++ 3.DDL-DCL/.scripts/participation.sh
@@ -47,7 +47,7 @@ do
        OK_SCHEMA="[:x:]"
    fi
    F_DATA=${id}/${id}-data.sql
-   if [ -f "$F_DATA" ]; then
+   if [[ -f "$F_DATA" && ! -s "$F_DATA" ]]; then
        OK_DATA="[:heavy_check_mark:](../${F_DATA})"
    else
        OK_DATA="[:x:]"
