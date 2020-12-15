0eaa637c64983cdb491b185a42c93aa8927af86b
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2020-12-15 00:56:40 -0500

    Update 300116670-data.sql

--- 3.DDL-DCL/300116670/300116670-data.sql
+++ 3.DDL-DCL/300116670/300116670-data.sql
@@ -6,14 +6,14 @@ INSERT INTO FOURNISSEURS (Nom, Coordonnees,Adresse) VALUES ('BERTAFREY','647-858
 
 -- donnees table produits
 
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Quad 4x4',2250,'standard')
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Goldwing',27000,'premium')
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Buggy 4x4',1000,'standard')
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Quad T3',7000,'premium')
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('SSV',10000,'premium')
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Panigale V4',1150,'standard')
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Harley Davidson',45000,'premium')
-INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,Type_du_produit) VALUES('Moto trial',3500,'standard')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('Quad 4x4',2250,'standard')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('Goldwing',27000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('Buggy 4x4',1000,'standard')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('Quad T3',7000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('SSV',10000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('Panigale V4',1150,'standard')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('Harley Davidson',45000,'premium')
+INSERT INTO PRODUITS (Nom_du_produit,Prix_du_produit,fournisseur) VALUES('Moto trial',3500,'standard')
 
 -- donnees table ventes
 
@@ -31,3 +31,20 @@ INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES
 INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Green','Desmond',647-270-2550,'57 Combermere drive',ngoma)
 INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Moe','Roger','647-547-5957,'101 Combermere drive')
 
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Kouta','Auriane','647-648-5657,'22 Garden avenue east')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Batchi','Ezechiel','647-250-8987','123 Baldwin street')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Ngoma','Christopher','647-470-2250','79 Gridwood street') 
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Green','Desmond',647-270-2550,'57 Combermere drive',ngoma)
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Moe','Roger','647-547-5957,'101 Combermere drive')
+
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Kouta','Auriane','647-648-5657,'22 Garden avenue east')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Batchi','Ezechiel','647-250-8987','123 Baldwin street')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Ngoma','Christopher','647-470-2250','79 Gridwood street') 
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Green','Desmond',647-270-2550,'57 Combermere drive',ngoma)
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Moe','Roger','647-547-5957,'101 Combermere drive')
+
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Kouta','Auriane','647-648-5657,'22 Garden avenue east')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Batchi','Ezechiel','647-250-8987','123 Baldwin street')
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Ngoma','Christopher','647-470-2250','79 Gridwood street') 
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Green','Desmond',647-270-2550,'57 Combermere drive',ngoma)
+INSERT INTO VENTES (Nom,Prenom,Coordonnées,Adresse,Adresse_electronique) VALUES('Moe','Roger','647-547-5957,'101 Combermere drive')
