ed85616525657b7d40c23c1c395ffc3eae2d0531
zoureni <56364857+zoureni@users.noreply.github.com>
2020-12-16 17:41:34 -0500

    Update 300118524-schema.sql

--- 3.DDL-DCL/300118524/300118524-schema.sql
+++ 3.DDL-DCL/300118524/300118524-schema.sql
@@ -10,111 +10,127 @@ GRANT ALL ON ALL_TELECOM.* TO 'zoureni'@'%';
 -- ACCÉDER À MON DATABASE 300118524
 USE ALL_TELECOM;
 
--- CRÉER TABLE FOURNISSEURS
-CREATE TABLE FOURNISSEURS (
-    fournisseur INT AUTO_INCREMENT,
-    nom VARCHAR(24) NOT NULL,
-    telephone NUMERIC NOT NULL,
-    email VARCHAR(24),
-    addresse VARCHAR(50),
-    PRIMARY KEY(fournisseur)
+-- -----------------------------------------------------
+-- Table ZONES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS ZONES (
+  zone INT NOT NULL AUTO_INCREMENT,
+  province VARCHAR(30) NULL DEFAULT NULL,
+  ville VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (zone),
+  INDEX zone (zone ASC) VISIBLE
+  );
+
+
+
+-- -----------------------------------------------------
+-- Table CLIENTS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS CLIENTS (
+  client INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(24) NOT NULL,
+  prenom VARCHAR(24) NOT NULL,
+  addresse VARCHAR(50) NOT NULL,
+  telephone DECIMAL(10,0) NOT NULL,
+  birthday DATE NULL DEFAULT NULL,
+  zone INT NOT NULL,
+  PRIMARY KEY (client, telephone, zone),
+  INDEX fk_CLIENTS_ZONES1_idx (zone ASC) VISIBLE,
+  CONSTRAINT fk_CLIENTS_ZONES1
+    FOREIGN KEY (zone)
+    REFERENCES ZONES (zone)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
     );
-    
--- CRÉER TABLE VITESSE
-CREATE TABLE VITESSES (
-    vitesse INT AUTO_INCREMENT,
-    download VARCHAR (8),
-    upload VARCHAR (8),
-    fournisseur INT,
-    PRIMARY KEY(vitesse)
-    );
-    
--- CRÉER TABLE PRODUITS
-    CREATE TABLE PRODUITS (
-produit INT AUTO_INCREMENT,
-cable BOOLEAN ,
-dsl BOOLEAN,
-fibre BOOLEAN,
-fournisseur INT,
-vitesse INT,
-PRIMARY KEY (produit, fournisseur),
+
+
+-- -----------------------------------------------------
+-- Table EQUIPEMENTS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS EQUIPEMENTS (
+  equipement INT NOT NULL AUTO_INCREMENT,
+  routeur BOOLEAN NULL DEFAULT NULL,
+  rj45 BOOLEAN NULL DEFAULT NULL,
+  coaxial BOOLEAN NULL DEFAULT NULL,
+  alimentation BOOLEAN NULL DEFAULT NULL,
+  PRIMARY KEY (equipement)
+  );
+
+
+-- -----------------------------------------------------
+-- Table FOURNISSEURS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS FOURNISSEURS (
+  fournisseur INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(24) NOT NULL,
+  telephone DECIMAL(10,0) NOT NULL,
+  email VARCHAR(24) NULL DEFAULT NULL,
+  addresse VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (fournisseur)
+);
+
+
+-- -----------------------------------------------------
+-- Table VITESSES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS VITESSES (
+  vitesse INT NOT NULL AUTO_INCREMENT,
+  download VARCHAR(8) NULL DEFAULT NULL,
+  upload VARCHAR(8) NULL DEFAULT NULL,
+  fournisseur INT NULL DEFAULT NULL,
+  prix FLOAT NULL DEFAULT NULL,
+  PRIMARY KEY (vitesse)
+  );
+
+
+-- -----------------------------------------------------
+-- Table PRODUITS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS PRODUITS (
+  produit INT NOT NULL AUTO_INCREMENT,
+  cable BOOLEAN NULL DEFAULT NULL,
+  dsl BOOLEAN NULL DEFAULT NULL,
+  fibre BOOLEAN NULL DEFAULT NULL,
+  fournisseur INT NOT NULL,
+  equipement INT NOT NULL,
+  vitesse INT NOT NULL,
+  PRIMARY KEY (produit, fournisseur, equipement, vitesse),
   INDEX fournisseur (fournisseur ASC) VISIBLE,
+  INDEX fk_PRODUITS_EQUIPEMENTS1_idx (equipement ASC) VISIBLE,
+  INDEX fk_PRODUITS_VITESSES1_idx (vitesse ASC) VISIBLE,
   CONSTRAINT PRODUITS_ibfk_1
     FOREIGN KEY (fournisseur)
     REFERENCES FOURNISSEURS (fournisseur)
     ON DELETE CASCADE,
-  INDEX fk_PRODUITS_VITESSES1_idx (vitesse ASC) VISIBLE,
+  CONSTRAINT fk_PRODUITS_EQUIPEMENTS1
+    FOREIGN KEY (equipement)
+    REFERENCES EQUIPEMENTS (equipement)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT fk_PRODUITS_VITESSES1
     FOREIGN KEY (vitesse)
     REFERENCES VITESSES (vitesse)
-    ON DELETE CASCADE
-    
-);
-
-    
-
--- CRÉER TABLE ZONE
-CREATE TABLE ZONES (
-    zone INT AUTO_INCREMENT,
-    province VARCHAR (30),
-    ville VARCHAR (30),
-    PRIMARY KEY(zone),
-    INDEX zone (zone ASC) VISIBLE
-    );
-    
-    
--- CRÉER TABLE ROUTEURS
-CREATE TABLE EQUIPEMENTS (
-    equipement INT AUTO_INCREMENT,
-    routeur BOOLEAN,
-    rj45 BOOLEAN,
-    coaxial BOOLEAN,
-    alimentation BOOLEAN,
-    fournisseur INT,
-    PRIMARY KEY(equipement),
-    FOREIGN KEY(fournisseur) REFERENCES FOURNISSEURS(fournisseur)
-    );  
-
--- CRÉER TABLE CLIENT
-CREATE TABLE CLIENTS (
-    client INT AUTO_INCREMENT,
-    nom VARCHAR(24) NOT NULL,
-    prenom VARCHAR(24) NOT NULL,
-    addresse VARCHAR(50) NOT NULL,
-    telephone NUMERIC NOT NULL,
-    zone INT,
-    PRIMARY KEY(client, telephone),
-    CONSTRAINT ZONES_ibfk_1
-      FOREIGN KEY(zone) REFERENCES ZONES(zone)
-    );
-
-    
--- CRÉER TABLE PRIX
-CREATE TABLE PRIX (
-    vitesse INT,
-    prix INT,
-    PRIMARY KEY(vitesse, prix),
-    CONSTRAINT PRIX_ibfk_1
-    FOREIGN KEY (vitesse)
-    REFERENCES VITESSES (vitesse)
-    );
-
--- CRÉER TABLE VENTES
-CREATE TABLE VENTES (
-vente INT,
-produit INT,
-client INT,
-telephone NUMERIC,
-PRIMARY KEY (vente,produit, client, telephone),
-INDEX fk_VENTES_CLIENTS1 (client ASC, telephone ASC) VISIBLE,
-INDEX fk_VENTES_PRODUITS1_idx (produit ASC) VISIBLE,
-  CONSTRAINT fk_VENTES_CLIENTS1
-    FOREIGN KEY (client, telephone)
-    REFERENCES CLIENTS (client, telephone),
-    CONSTRAINT fk_VENTES_PRODUITS1
-    FOREIGN KEY (produit)
-    REFERENCES PRODUITS (produit)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
-);
+    );
+
+
+-- -----------------------------------------------------
+-- Table VENTES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS VENTES (
+  vente INT NOT NULL,
+  produit INT NOT NULL,
+  client INT NOT NULL,
+  telephone DECIMAL(10,0) NOT NULL,
+  PRIMARY KEY (vente, produit, client, telephone),
+  INDEX fk_VENTES_CLIENTS1 (client ASC, telephone ASC) VISIBLE,
+  INDEX fk_VENTES_PRODUITS1_idx (produit ASC) VISIBLE,
+  CONSTRAINT fk_VENTES_CLIENTS1
+    FOREIGN KEY (client , telephone)
+    REFERENCES CLIENTS (client , telephone),
+  CONSTRAINT fk_VENTES_PRODUITS1
+    FOREIGN KEY (produit)
+    REFERENCES PRODUITS (produit)
+    );
 
